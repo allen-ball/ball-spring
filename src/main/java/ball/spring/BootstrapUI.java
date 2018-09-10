1645635b1896ecd819d1152c535b7b59f1fd8f1b
Allen D. Ball <ball@hcf.dev>
2018-09-10 02:30:16 +0000

    * src/main/java/ball/spring/BootstrapUI.java: 	Created Controller abstract base class with associated template; 	implementations should provide menu.html and body.html templates 	and override/implement the @ModelAttribute methods as necessary.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.spring/trunk@3316 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/spring/BootstrapUI.java
@@ -0,0 +1,81 @@
+/*
+ * $Id$
+ *
+ * Copyright 2018 Allen D. Ball.  All rights reserved.
+ */
+package ball.spring;
+
+import java.util.NoSuchElementException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+/**
+ * Bootstrap UI {@link org.springframework.stereotype.Controller} abstract
+ * base class
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+public abstract class BootstrapUI {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    /**
+     * View name for this template.
+     */
+    protected static final String VIEW = BootstrapUI.class.getSimpleName();
+
+    /**
+     * Sole constructor.
+     */
+    protected BootstrapUI() { }
+
+    @ModelAttribute("css")
+    public String[] css() { return new String[] { }; }
+
+    @ModelAttribute("js")
+    public String[] js() { return new String[] { }; }
+
+    @ModelAttribute("brand")
+    public String brand() { return null; }
+
+    @ModelAttribute("template")
+    public abstract String template();
+
+    @ExceptionHandler
+    @ResponseStatus(value = NOT_FOUND)
+    public String handleNOT_FOUND(Model model,
+                                  NoSuchElementException exception) {
+        populate(model, exception);
+
+        return VIEW;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = INTERNAL_SERVER_ERROR)
+    public String handleINTERNAL_SERVER_ERROR(Model model,
+                                              Exception exception) {
+        populate(model, exception);
+
+        return VIEW;
+    }
+
+    private void populate(Model model, Exception exception) {
+        model.addAttribute("css", css());
+        model.addAttribute("js", js());
+        model.addAttribute("brand", brand());
+        model.addAttribute("template", template());
+
+        model.addAttribute("exception", exception);
+    }
+
+    @Override
+    public String toString() { return super.toString(); }
+}
