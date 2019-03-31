e1cabad06ab3960dad54cf2733352cead5283af6
Allen D. Ball <ball@hcf.dev>
2019-03-31 03:38:09 +0000

    Fixed spelling error.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.spring/trunk@3975 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/spring/package-info.java
+++ src/main/java/ball/spring/package-info.java
@@ -4,7 +4,7 @@
  * Copyright 2019 Allen D. Ball.  All rights reserved.
  */
 /**
- * Reusable {@link.uri https://spring.io/ Srping} components.
+ * Reusable {@link.uri https://spring.io/ Spring} components.
  *
  * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
  */
