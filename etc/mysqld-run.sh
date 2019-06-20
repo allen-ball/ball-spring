853b4b859b5083225bb1333d9a30ac5e0357a510
Allen D. Ball <ball@hcf.dev>
2019-06-20 21:16:31 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.spring/trunk@4217 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ etc/mysqld-run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# $Id$
+
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+    ls=$(ls -ld "$PRG")
+    link=$(expr "$ls" : '.*-> \(.*\)$')
+    if expr "$link" : '/.*' > /dev/null; then
+        PRG="$link"
+    else
+        PRG=$(dirname "$PRG")"/$link"
+    fi
+done
+
+cd $(dirname "$PRG")
+
+MYCNF=$(pwd)/my.cnf
+DATADIR=$(pwd)/data
+SOCKET=$(pwd)/socket
+
+DEFAULTS_OPT=--no-defaults
+DATADIR_OPT=--datadir="${DATADIR}"
+
+if [ -f "${MYCNF}" ]; then
+    DEFAULTS_OPT=--defaults-file="${MYCNF}"
+fi
+
+if [ ! -d "${DATADIR}" ]; then
+    mysqld "${DEFAULTS_OPT}" "${DATADIR_OPT}" --initialize-insecure
+fi
+
+exec mysqld "${DEFAULTS_OPT}" "${DATADIR_OPT}" --socket="${SOCKET}"
