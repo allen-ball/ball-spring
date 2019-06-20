8d3074d1d0d7aacc70d9d21688db50d9d8c08cd4
Allen D. Ball <ball@hcf.dev>
2019-06-20 21:00:06 +0000

    Removed support for mysql_upgrade which is now deprecated.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.spring/trunk@4216 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/spring/mysqld/MysqldConfiguration.java
+++ src/main/java/ball/spring/mysqld/MysqldConfiguration.java
@@ -121,19 +121,20 @@ public class MysqldConfiguration {
                             throw new IllegalStateException("mysqld not started");
                         }
                     }
-
-                    try {
-                        new ProcessBuilder("mysql_upgrade",
-                                           defaultsArg, socketArg,
-                                           "--user=root")
-                            .directory(home)
-                            .inheritIO()
-                            .redirectOutput(Redirect.appendTo(console))
-                            .redirectErrorStream(true)
-                            .start()
-                            .waitFor();
-                    } catch (InterruptedException exception) {
-                    }
+                    /*
+                     * try {
+                     *     new ProcessBuilder("mysql_upgrade",
+                     *                        defaultsArg, socketArg,
+                     *                        "--user=root")
+                     *     .directory(home)
+                     *     .inheritIO()
+                     *     .redirectOutput(Redirect.appendTo(console))
+                     *     .redirectErrorStream(true)
+                     *     .start()
+                     *     .waitFor();
+                     * } catch (InterruptedException exception) {
+                     * }
+                     */
                 } else {
                     throw new IllegalStateException("mysqld datadir does not exist");
                 }
