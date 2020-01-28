1b5e8704f48b83b13ddabb30d8c7aeee8f0675cd
Allen D. Ball <ball@hcf.dev>
2020-01-28 17:22:01 +0000

    Converted from EntityManagerFactoryConfiguration per latest Spring Boot recommendations.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.spring/trunk@5161 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/spring/mysqld/EntityManagerFactoryComponent.java
+++ src/main/java/ball/spring/mysqld/EntityManagerFactoryComponent.java
@@ -1,19 +1,20 @@
 /*
  * $Id$
  *
- * Copyright 2018, 2019 Allen D. Ball.  All rights reserved.
+ * Copyright 2018 - 2020 Allen D. Ball.  All rights reserved.
  */
 package ball.spring.mysqld;
 
+import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.jpa.EntityManagerFactoryDependsOnPostProcessor;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 /**
- * {@link EntityManagerFactoryDependsOnPostProcessor} {@link Configuration}
+ * {@link EntityManagerFactoryDependsOnPostProcessor} {@link Component}
  * implementation.  See
  * {@link.uri https://docs.spring.io/spring-boot/docs/current/reference/html/howto-data-access.html#howto-configure-a-component-that-is-used-by-JPA target=newtab Configure a Component that is Used by JPA}.
  *
@@ -22,17 +23,15 @@ import static org.apache.commons.lang3.StringUtils.EMPTY;
  * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
  * @version $Revision$
  */
-@Configuration
+@Component
 @ConditionalOnProperty(name = "mysqld.home", havingValue = EMPTY)
-public class EntityManagerFactoryConfiguration
+@ToString
+public class EntityManagerFactoryComponent
              extends EntityManagerFactoryDependsOnPostProcessor {
     @Autowired private Process mysqld;
 
     /**
      * Sole constructor.
      */
-    public EntityManagerFactoryConfiguration() { super("mysqld"); }
-
-    @Override
-    public String toString() { return super.toString(); }
+    public EntityManagerFactoryComponent() { super("mysqld"); }
 }
