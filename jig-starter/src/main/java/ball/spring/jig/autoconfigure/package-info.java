192f7113778a8252cdd877b119df9edbe53e9360
Allen D. Ball <ball@hcf.dev>
2020-09-04 18:03:19 +0000

    Re-organized ball-spring-*-starter modules within ball-spring source hierarchy.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball-spring/trunk@6738 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ jig-starter/src/main/java/ball/spring/jig/autoconfigure/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Ball Spring Jig Auto Configuration Classes.
+ *
+ * {@pom.coordinates}
+ *
+ * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
+ */
+@Manifest.Section
+package ball.spring.jig.autoconfigure;
+
+import ball.annotation.Manifest;
