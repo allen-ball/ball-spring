54a1572362f740cfb796a778d99ba43b124f1c47
Allen D. Ball <ball@hcf.dev>
2020-09-13 00:39:11 +0000

    Support #webjars.assets().
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball-spring/trunk@6760 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- jar/src/main/java/ball/spring/dialect/WebJarsDialect.java
+++ jar/src/main/java/ball/spring/dialect/WebJarsDialect.java
@@ -21,7 +21,9 @@ package ball.spring.dialect;
  * ##########################################################################
  */
 import ball.annotation.CompileTimeCheck;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -31,6 +33,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.util.AntPathMatcher;
 import org.thymeleaf.IEngineConfiguration;
 import org.thymeleaf.context.IExpressionContext;
 import org.thymeleaf.context.ITemplateContext;
@@ -47,11 +50,12 @@ import org.thymeleaf.standard.expression.IStandardExpressionParser;
 import org.thymeleaf.standard.expression.StandardExpressions;
 import org.webjars.WebJarAssetLocator;
 
+import static java.util.stream.Collectors.toCollection;
 import static java.util.stream.Collectors.toSet;
 import static org.thymeleaf.templatemode.TemplateMode.HTML;
 
 /**
- * {@link ball.spring.expression.WebJars} Thymeleaf dialect.
+ * {@link WebJarAssetLocator} Thymeleaf dialect.
  *
  * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
  * @version $Revision$
@@ -139,7 +143,7 @@ public class WebJarsDialect extends AbstractProcessorDialect
                 .execute(context);
 
             try {
-                path = path(locator, true, scheme, path);
+                path = path(locator, false, scheme, path);
             } catch (IllegalArgumentException exception) {
             }
 
@@ -173,7 +177,51 @@ public class WebJarsDialect extends AbstractProcessorDialect
      */
     @NoArgsConstructor @ToString
     public static class WebJars {
+        @CompileTimeCheck
+        private static final Pattern PATTERN =
+            Pattern.compile("(?i)[\\p{Space},]+");
+
         private final WebJarAssetLocator locator = new WebJarAssetLocator();
+        private final Set<String> assets = locator.listAssets();
+        private final AntPathMatcher matcher = new AntPathMatcher();
+
+        /**
+         * Method to return WebJar resources matching {@link AntPathMatcher}
+         * patterns.
+         *
+         * @param   patterns        The {@link AntPathMatcher} patterns to
+         *                          match.
+         *
+         * @return  The matching resource paths.
+         */
+        public Collection<String> assets(String... patterns) {
+            return assets(false, null, patterns);
+        }
+
+        /**
+         * Method to return WebJar resources matching {@link AntPathMatcher}
+         * patterns.
+         *
+         * @param   useCdn          {@code true} to provide a CDN
+         *                          {@code URI}; {@code false} for a local
+         *                          path.
+         * @param   scheme          The {@code URI} scheme.
+         * @param   patterns        The {@link AntPathMatcher} patterns to
+         *                          match.
+         *
+         * @return  The matching resource paths.
+         */
+        public Collection<String> assets(boolean useCdn, String scheme,
+                                         String... patterns) {
+            Collection<String> collection =
+                Stream.of(patterns)
+                .flatMap(t -> PATTERN.splitAsStream(t))
+                .flatMap(t -> assets.stream().filter(a -> matcher.match(t, a)))
+                .map(t -> path(locator, useCdn, scheme, t))
+                .collect(toCollection(LinkedHashSet::new));
+
+            return collection;
+        }
 
         /**
          * Method to convert a WebJar resource (partial) path to its
